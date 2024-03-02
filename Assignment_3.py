7cd1e699d482b1b444687592264af7f62815a669
Abhinav2488 <161736622+Abhinav2488@users.noreply.github.com>
2024-03-02 20:59:31 +0530

    Solved assignment file is uploaded

---
+++ Assignment_3.py
@@ -0,0 +1,65 @@
+#--------------- Q 1 ----------------------------------------------------
+
+'''Keywords are the reserved words that can not
+be used as a variable, function or any other identifier.
+Examples - 1. for 
+           2. in
+           3. is
+           4. break
+           5. continue  '''
+
+#---------------- Q 2 ----------------------------------------------------
+# ---- Rules for defining identifiers---------------
+
+'''
+1. Identifiers cannot be a keyword.
+2. It can have a sequence of letters and digits.
+3. We cannot use special symbols like !, @, #, $, and so on.
+4. Identifiers are case-sensitive.
+5. It's a convention to start an identifier with a letter rather _.
+6. Whitespaces are not allowed.
+
+Example - counter_ = 5
+
+'''
+
+# ------------- Q 3 -------------------------------------------------------
+
+''' Comments in Python are the lines in the code that are ignored by
+the interpreter during the execution of the program. 
+Comments increase understandibility and readbility in a piece of code
+'''
+'''Example - 
+1. Single line comment :--- # Its a single line comment'''
+'''2. Multi line comment :--- ''' ''' its a 
+                                        multi line comment'''
+
+# -------------- Q 4 --------------------------------------------------------
+
+''' In python Indentation represents a block of code which is to be executed according to usage.
+For example 
+
+for i in range(5):
+    print(i)
+    if i == 4:
+        pass
+print("executed")         
+    
+in the above code print statement and if condition is in the loop that represents a whole 
+block of code and second print statement is out of the loop or out of code block which is
+decided by indentation '''
+
+# --------------------------- Q 5 -------------------------------------------
+
+''' If indentation is incorrect  then code will not be executed, interpreter will show error'''
+
+# ---------------------------- Q 6 --------------------------------------------
+
+'''
+An Expression is a sequence or combination of values, variables, operators and function
+calls that always produces or returns a result value
+Example :-- x = y + 5
+
+Any Instruction that a python interpreter can execute (carry out) is called a Statement.
+x = 5
+print(x)'''
